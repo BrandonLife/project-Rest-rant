55cf5ed80b3f4429da01e82793b9e24890c01919
Brandon Life <blife1119@gmail.com>
2023-07-15 14:08:35 -0400

    finished assignment, all crud working, css has been fixed

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -11,26 +11,26 @@ function edit_form (data) {
           <div className="row">
             <div className="form-group col-sm-6">
               <label htmlFor="name">Place Name</label>
-              <input id="name" name="name" defaultValue={data.place.name} />
+              <input id="name" name="name" className="form-control" defaultValue={data.place.name} />
               </div>
               <div className="form-group col-sm-6">
               <label htmlFor="pic">Place Picture</label>
-              <input id="pic" name='pic' defaultValue={data.place.pic}/>
+              <input id="pic" name='pic' className="form-control" defaultValue={data.place.pic}/>
               </div>
               <div className="form-group col-sm-6">
               <label htmlFor="city">City</label>
-              <input id="city" name='city' defaultValue={data.place.city}/>
+              <input id="city" name='city' className="form-control" defaultValue={data.place.city}/>
               </div>
               <div className="form-group col-sm-6">
               <label htmlFor="state">State</label>
-              <input id="state" name='state' defaultValue={data.place.state}/>
+              <input id="state" name='state' className="form-control" defaultValue={data.place.state}/>
               </div>
               <div className="form-group col-sm-6">
               <label htmlFor="cuisines">Cusines</label>
-              <input id="cuisines" name='cuisines' defaultValue={data.place.cuisines}/>
+              <input id="cuisines" name='cuisines' className="form-control" defaultValue={data.place.cuisines}/>
               </div>
           </div>
-          <input className="btn btn-primary" type="submit" value="Add Place" />
+          <input className="btn btn-primary" type="submit" value="Update Place" />
             </form>
           </main>
         </Def>
